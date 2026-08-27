54d31cdf4b61f899409b9ef606c24544f709973f
Anderson Fuel <camis.89f@gmail.com>
2026-08-27 18:06:16 -0500

    Add the first part on types.

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/home/')
+@app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/types/')
+def types():
+    return render_template('types.html')
+
